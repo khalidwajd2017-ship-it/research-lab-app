92a9313023f29aed67b8f64615360fb5ff0ff060
khalidwajd2017-ship-it <khalidwajd2017@gmail.com>
2025-12-26 20:28:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(page_title="منصة التميز البحثي", layout="wide
 # استبدل [YOUR-PASSWORD] بكلمة مرورك: 8?Q4.G/iLe84d-j
 # مثال على شكل الرابط: postgres://postgres.xxxx:pass@aws-0-eu-central-1.pooler.supabase.com:6543/postgres
 
-DB_CONNECTION_STRING = "postgres://postgres.jecmwuiqofztficcujpe:khalidcom_1981@aws-0-eu-central-1.pooler.supabase.com:6543/postgres?sslmode=require"
+DB_CONNECTION_STRING = "postgresql://postgres.jecmwuiqofztficcujpe:khalidcom_1981@aws-1-eu-west-2.pooler.supabase.com:6543/postgres"
 
 # ملاحظة: قمتُ بمحاولة تخمين الرابط وتشفير كلمة السر لك في السطر أعلاه
 # إذا لم يعمل، احذفه وضع الرابط الذي نسخته أنت يدوياً
@@ -173,4 +173,5 @@ else:
         st.dataframe(df)
     else:
         st.info("سجل الأعمال فارغ حالياً. ابدأ بإضافة نتاج علمي جديد.")
+
 
