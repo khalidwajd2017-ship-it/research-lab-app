b2645f8b5afe3d036eb40a8cd21608ec45795dcd
khalidwajd2017-ship-it <khalidwajd2017@gmail.com>
2026-01-04 23:18:44 +0100

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,18 @@
+from app.database import SessionLocal
+from app.models import User
+import bcrypt
 
+def auth_user(u, p):
+    s = SessionLocal()
+    try:
+        user = s.query(User).filter(User.username == u).first()
+        if user and bcrypt.checkpw(p.encode(), user.password_hash.encode()):
+            return user
+    except Exception as e:
+        pass
+    finally:
+        s.close()
+    return None
+
+def register_user_secure(u, f, p, role, code, t_id, d_id, m_type):
+    # استكمال الكود هنا مع إضافة خاصية التسجيل
