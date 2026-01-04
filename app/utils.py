9b16ab62f2de411c79644ac03a6ed995881fe685
khalidwajd2017-ship-it <khalidwajd2017@gmail.com>
2026-01-04 23:19:28 +0100

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,12 @@
+import base64
+import os
+
+def get_img_as_base64(file_path):
+    try:
+        with open(file_path, "rb") as f: data = f.read()
+        return base64.b64encode(data).decode()
+    except Exception as e:
+        return None
+
+# استكمال بعض الدوال المساعدة الأخرى
 
